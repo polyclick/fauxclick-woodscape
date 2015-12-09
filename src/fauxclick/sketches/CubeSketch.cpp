cda40d699b12adf553b4c7d77790aaf409aa39b2
Matthias <matthias@lunargravity.be>
2015-12-09 01:25:35 +0100

    cubes

--- src/fauxclick/sketches/CubeSketch.cpp
+++ src/fauxclick/sketches/CubeSketch.cpp
@@ -10,8 +10,16 @@ CubeSketch::~CubeSketch(){
 }
 
 void CubeSketch::setup() {
-  grid.enablePulse();
-  grid.enableTransform(0.9, 0.9);
+  this->stepCounter = 0;
+  this->cooldown = 0;
+  
+  this->cLeft = ofColor(6, 103, 111);
+  this->cRight = ofColor(70, 186, 177);
+  this->cTop = ofColor(158, 224, 208);
+  
+  this->cDark = ofColor(6, 103, 111);
+  this->cMid = ofColor(70, 186, 177);
+  this->cBright = ofColor(158, 224, 208);
 }
 
 void CubeSketch::update() {
@@ -22,45 +30,80 @@ void CubeSketch::draw() {
   ofBackground(0);
   ofFill();
   
-  for(int i=-1; i<14; i+=2) {
+  for(int i=0; i<17; i+=2) {
     for(int j=10; j>0 ; j-=3){
       
-      int delay = 1.5*j;
+      int delay = 20 - 1.5*j;
 
       if (j%2 == 0) {
         if (this->app->audioManager->beatReceived) {
-          pulseCube(i, i+1, j, delay);
+          if(this->cooldown<=0){
+            shiftColor();
+            this->cooldown = 25;
+          }
         }
         drawCube(i, i+1, j);
       }else{
         if (this->app->audioManager->beatReceived) {
-          pulseCube(i-1, i, j, delay);
+          shiftColor();
         }
         drawCube(i-1, i, j);
       }
     }
   }
 
-
-  grid.decay();
+  this->cooldown-= 1;
+//  grid.decay();
 }
 
 void CubeSketch::drawCube( int colOdd, int colEven, int rowBottom) {
   
   // Dark shade
-  ofSetColor(6, 103, 111);
+  ofSetColor(this->cLeft);
   grid.drawFace(rowBottom, colOdd);
   grid.drawFace(rowBottom - 1, colOdd);
   
   // Mid shade
-  ofSetColor(70, 186, 177);
+  ofSetColor(this->cRight);
   grid.drawFace(rowBottom, colEven);
   grid.drawFace(rowBottom - 1, colEven);
   
   // Bright shade
-  ofSetColor(158, 224, 208);
+  ofSetColor(this->cTop);
   grid.drawFace(rowBottom - 2, colOdd);
   grid.drawFace(rowBottom - 2, colEven);
+}
+
+void CubeSketch::shiftColor(){
+  
+  switch (this->stepCounter) {
+      cout << this->stepCounter << endl;
+      
+    case 0:
+      cout << "step0" << endl;
+      this->cLeft = this->cDark;
+      this->cTop =this->cBright;
+      this->cRight = this->cMid;
+      break;
+      
+    case 1:
+      cout << "step1" << endl;
+      this->cLeft = this->cMid;
+      this->cTop =this->cDark;
+      this->cRight = this->cBright;
+      break;
+      
+    case 2:
+      cout << "step2" << endl;
+      this->cLeft = this->cBright;
+      this->cTop =this->cMid;
+      this->cRight = this->cDark;
+      break;
+  }
+  
+  this->stepCounter++;
+
+  if(this->stepCounter > 1 ){ this->stepCounter = 0; }
 }
 
 void CubeSketch::pulseCube( int colOdd, int colEven, int rowBottom, int delay) {
--- src/fauxclick/sketches/CubeSketch.h
+++ src/fauxclick/sketches/CubeSketch.h
@@ -9,16 +9,27 @@ class CubeSketch : public SketchBase {
   protected:
 
     void drawCube(int colOdd, int colEven, int rowBottom);
+    void shiftColor();
     void pulseCube(int colOdd, int colEven, int rowBottom, int delay=0);
 
-
+    int stepCounter;
+    int cooldown;
+  
+    ofColor cDark;
+    ofColor cMid;
+    ofColor cBright;
+  
+    ofColor cLeft;
+    ofColor cRight;
+    ofColor cTop;
+  
     const char* name;
     ofApp* app;
     Grid grid;
 
     // Set unit width
     const int unitWidth = 101;
-
+ 
     // Set unit height
     const int unitHeight = 118;
 
