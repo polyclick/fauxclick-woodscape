8488d3b8edf3bc9b7a1f501d207b0e16e187e109
Bart Claessens <bart.cla@gmail.com>
2015-12-09 22:38:32 +0100

    Volume history sketch

--- src/fauxclick/sketches/VolumeHistorySketch.cpp
+++ src/fauxclick/sketches/VolumeHistorySketch.cpp
@@ -12,50 +12,128 @@ VolumeHistorySketch::~VolumeHistorySketch(){
 }
 
 void VolumeHistorySketch::setup(){
+  
+  // glitch
+  myFbo.allocate(ofGetWidth(), ofGetHeight());
+  myGlitch.setup(&myFbo);
 
   // vector to keep the volume changes
   volumeHistory.assign(volumeHistorySize, 0.0);
+  
+  // image paths
+  vector<const char*> paths;
+  paths.push_back("images/landscape-01.jpg");
+  paths.push_back("images/landscape-02.jpg");
+  paths.push_back("images/landscape-03.jpg");
+  paths.push_back("images/landscape-04.jpg");
+  paths.push_back("images/landscape-05.jpg");
+  //paths.push_back("images/tits.jpg");
+  
+  // load images
+  for(int i = 0 ; i < paths.size() ; i++) {
+    ofImage image;
+    image.load(paths[i]);
+    images.push_back(image);
+  }
+  
+  // set first image
+  currentImage = images[0];
 }
 
 void VolumeHistorySketch::update(){
-
+  if(ofGetElapsedTimeMillis() - elaspedSinceLastBeatOnset > 50) {
+    myGlitch.setFx(OFXPOSTGLITCH_CUTSLIDER, false);
+    myGlitch.setFx(OFXPOSTGLITCH_INVERT, false);
+  }
 
   // lets record the volume into an array
   volumeHistory.push_back(this->app->audioManager->normalizedVolume);
-  //cout << this->app->audioManager->beat.getMagnitude() << endl;
 
   // if we are bigger the the size we want to record - lets drop the oldest value
   if(volumeHistory.size() >= volumeHistorySize){
     volumeHistory.erase(volumeHistory.begin(), volumeHistory.begin() + 1);
   }
+
+  // onset detected, pick random image
+  if(this->app->audioManager->onsetReceived) {
+    onsetCounter++;
+    if(onsetCounter == 3) {
+      onsetCounter = 0;
+      cout << "change" << endl;
+      int randomIndex = rand() % images.size();
+      currentImage = images[randomIndex];
+    }
+  }
+  
+  // kick detected
+  if(this->app->audioManager->beatReceived) {
+    beatCounter++;
+    if(beatCounter == 1) {
+      beatCounter = 0;
+      myGlitch.setFx(ofRandom(1.0) > 0.5 ? OFXPOSTGLITCH_CUTSLIDER : OFXPOSTGLITCH_INVERT, true);
+      elaspedSinceLastBeatOnset = ofGetElapsedTimeMillis();
+    }
+  }
+  
+  // draw to fbo
+  myFbo.begin();
+  ofClear(0, 0, 0, 255);
+  
+  // push
+  ofPushStyle();
+  ofPushMatrix();
+  
+  // draw bg image
+  int imgWidth = ofGetWidth();
+  int imgHeight = currentImage.getHeight() * ofGetWidth() / currentImage.getWidth();
+  currentImage.draw((ofGetWidth() - imgWidth) / 2.0, (ofGetHeight() - imgHeight) / 2.0, imgWidth, imgHeight);
+  
+  // scaling in width of screen
+  float widthRatio = (float)ofGetWidth() / ((float)volumeHistory.size() - 1);
+  
+  // set color
+  ofFill();
+  ofSetColor(0);
+  
+  // top graph
+  ofBeginShape();
+  for (unsigned int i = 0; i < volumeHistory.size(); i++) {
+    if(i == 0)
+      ofVertex(0, 0);
+    
+    float volume = volumeHistory[i] * ofGetHeight() * 1.5;
+    ofVertex(i * widthRatio, volume);
+    
+    if(i == volumeHistory.size() - 1)
+      ofVertex(i * widthRatio, 0);
+  }
+  ofEndShape(true);
+  
+  // bottom graph
+  ofBeginShape();
+  for (unsigned int i = 0; i < volumeHistory.size(); i++) {
+    if(i == 0)
+      ofVertex(0, ofGetHeight());
+    
+    float volume = volumeHistory[i] * ofGetHeight() * 1.5;
+    ofVertex(i * widthRatio, ofGetHeight() - volume);
+    
+    if(i == volumeHistory.size() - 1)
+      ofVertex(i * widthRatio, ofGetHeight());
+  }
+  ofEndShape(true);
+  
+  // pop em
+  ofPopMatrix();
+  ofPopStyle();
+  
+  // end drawing to fbo
+  myFbo.end();
 }
 
 void VolumeHistorySketch::draw() {
-  // draw the average volume:
-  ofPushStyle();
-  ofPushMatrix();
-
-  int height = ofGetHeight();
-
-  // scaling in width of screen
-  float widthRatio = (float)ofGetWidth() / (float)volumeHistory.size();
-
-  ofTranslate(0.0, 0, 0);
-
-		//lets draw the volume history as a graph
-		ofBeginShape();
-		for (unsigned int i = 0; i < volumeHistory.size(); i++){
-      if( i == 0 ) ofVertex(0, height);
-
-      float volume = volumeHistory[i] * ofGetHeight() / 2.0;
-      ofVertex(i * widthRatio, height - volume);
-
-      if(i == volumeHistory.size() - 1) ofVertex(i * widthRatio, height);
-    }
-		ofEndShape(false);
-
-		ofPopMatrix();
-  ofPopStyle();
+  myGlitch.generateFx();
+  myFbo.draw(0, 0);
 }
 
 const char* VolumeHistorySketch::getName() {
--- src/ofApp.h
+++ src/ofApp.h
@@ -64,4 +64,5 @@ public:
 
   ofxMidiIn midiIn;
   ofxMidiMessage midiMessage;
+
 };
